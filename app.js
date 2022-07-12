a4a8abb2555da9646cdd07ed875c8c657b3bd9a1
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2022-07-12 16:44:59 +0530

    Hash function usage

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 mongoose.connect("mongodb://localhost:27017/newuserDB");
 
@@ -18,8 +19,8 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+// const secret = process.env.SECRET;
+// userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
 
 const User = new mongoose.model('User', userSchema);
 
@@ -33,7 +34,7 @@ app.get('/login', (req, res) => {
 
 app.post('/login', (req, res) => {
     const userName = req.body.username
-    const password = req.body.password
+    const password = md5(req.body.password)
     User.findOne({ email: userName }, (err, foundUser) => {
         if (err)
             console.log(err);
@@ -56,7 +57,7 @@ app.get('/register', (req, res) => {
 app.post('/register', (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save((err) => {
         if (err)
